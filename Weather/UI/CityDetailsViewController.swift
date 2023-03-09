18fdb0562c99edd370e916e0567577a87684e0eb
Jerome Cabanis <jerome@letdevbegin.com>
2023-03-09 16:19:35 +0100

    First commit

---
+++ Weather/UI/CityDetailsViewController.swift
@@ -0,0 +1,28 @@
+//
+//  CityDetailsViewController.swift
+//  Meteo
+//
+//  Created by Jérôme Cabanis on 09/03/2023.
+//
+
+import UIKit
+
+class CityDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+		title = "Issy les Moulineaux"
+	}
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Weather/UI/CityListTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Meteo
+//
+//  Created by Jérôme Cabanis on 09/03/2023.
+//
+
+import UIKit
+
+class CityListTableViewController: UITableViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		title = NSLocalizedString("Weather", comment: "App name")
+	}
+
+
+}
+
